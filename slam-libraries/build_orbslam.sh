e822f70d1781d2fbf8f129e6040948eba7b1f686
john <john@viam.com>
2022-05-05 21:00:17 +0100

    add custom slam pathing

---
+++ slam-libraries/build_orbslam.sh
@@ -0,0 +1,47 @@
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd ORB_SLAM3/Thirdparty/DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+
+cd ../../Sophus
+
+echo "Configuring and building Thirdparty/Sophus ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+
+cd ../../../
+
+echo "Uncompress vocabulary ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
+
+echo "Configuring and building ORB_SLAM3 ..."
+cd ../ORB_SLAM3
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+
+cd ../../ORB_SLAM_CUSTOM
+mkdir bin
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
