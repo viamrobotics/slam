9ef67cdb8707af4c9ae4118a3b88e5d70bd4184e
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-06-22 06:42:00 -0400

    Make bash scripts safer to execute

--- slam-libraries/viam-cartographer/scripts/build_viam_cartographer.sh
+++ slam-libraries/viam-cartographer/scripts/build_viam_cartographer.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit
+
+if [ "$(uname)" == "Linux" ]; then
+    cd $(dirname $(readlink -f "${BASH_SOURCE}"))/..
+elif [ "$(uname)" == "Darwin" ]; then
+    readlinkorreal() { readlink "$1" || echo "$1"; }
+    cd $(dirname string readlinkorreal "${BASH_SOURCE}")/..
+else
+    echo ERROR: osx not implemented yet.
+    exit 1
+fi
 
 # Build and install Cartographer with the viam wrapper.
 rm -rf build
--- slam-libraries/viam-cartographer/scripts/run_cartographer.sh
+++ slam-libraries/viam-cartographer/scripts/run_cartographer.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit
-# set -o verbose
 
+if [ "$(uname)" == "Linux" ]; then
+    cd $(dirname $(readlink -f "${BASH_SOURCE}"))/..
+elif [ "$(uname)" == "Darwin" ]; then
+    readlinkorreal() { readlink "$1" || echo "$1"; }
+    cd $(dirname string readlinkorreal "${BASH_SOURCE}")/..
+else
+    echo ERROR: osx not implemented yet.
+    exit 1
+fi
 
 # ---- Edit based on your needs:
 DATE="June01"
