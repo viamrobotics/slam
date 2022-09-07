cffd6644add58d7f23c25999bbee17967c007709
John <john@viam.com>
2022-09-07 17:55:48 -0400

    add missing dependencies found with docker

--- slam-libraries/viam-orb-slam3/setup_orbslam.sh
+++ slam-libraries/viam-orb-slam3/setup_orbslam.sh
@@ -1,2 +1,2 @@
 echo "Installing ORB_SLAM3 external dependencies"
-sudo apt install libopencv-dev libeigen3-dev libssl-dev libboost-all-dev libpangolin-dev
+sudo apt install cmake libglew-dev libopencv-dev libeigen3-dev libssl-dev libboost-all-dev libpangolin-dev
