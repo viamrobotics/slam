b254537974498bf5a67d6befab0f726e55a3f028
Tess Avitabile <tess.avitabile@gmail.com>
2022-09-09 19:01:33 +0100

    remove license information

--- slam-libraries/viam-orb-slam3/orbslam_server_v1_main.cc
+++ slam-libraries/viam-orb-slam3/orbslam_server_v1_main.cc
@@ -1,23 +1,3 @@
-/**
- * This file is part of ORB-SLAM3
- *
- * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez
- * Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
- * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós,
- * University of Zaragoza.
- *
- * ORB-SLAM3 is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * ORB-SLAM3. If not, see <http://www.gnu.org/licenses/>.
- */
 #include <orbslam_server_v1.h>
 
 #include <algorithm>
@@ -131,7 +111,8 @@ int main(int argc, char **argv) {
     }
 
     std::unique_ptr<int> selected_port = std::make_unique<int>(0);
-    builder.AddListeningPort(slam_port, grpc::InsecureServerCredentials(), selected_port.get());
+    builder.AddListeningPort(slam_port, grpc::InsecureServerCredentials(),
+                             selected_port.get());
     builder.RegisterService(&slamService);
 
     // Start the SLAM gRPC server
