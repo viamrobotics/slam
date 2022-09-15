a72259870a0a7ccc6a80ec9ca0de30574c42e356
Katharina Kufieta <katharina@viam.com>
2022-09-15 14:46:38 +0100

    Fix bugs

--- slam-libraries/viam-orb-slam3/orbslam_server_v1.cc
+++ slam-libraries/viam-orb-slam3/orbslam_server_v1.cc
@@ -442,7 +442,7 @@ void SLAMServiceImpl::process_rgbd_offline(ORB_SLAM3::System *SLAM) {
     // file was generated
     int locClosest = -1;
     locClosest = utils::parseBothDataDir(path_to_data, filesRGB,
-                                         utils::FileParserMethod::Recent,
+                                         utils::FileParserMethod::Closest,
                                          yamlTime, &fileTimeStart);
     if (locClosest == -1) {
         BOOST_LOG_TRIVIAL(error) << "No new images to process in directory";
@@ -564,16 +564,19 @@ void SLAMServiceImpl::save_atlas_as_osa_with_timestamp(
             }
             return;
         }
-        {
+        bool could_not_save_map = false;
+        if (SLAM->GetAtlas()->GetCurrentMap()->GetAllKeyFrames().size() != 0) {
             std::lock_guard<std::mutex> lock(slam_mutex);
             SLAM->SaveAtlasAsOsaWithTimestamp(path_save_file_name);
+        } else {
+            could_not_save_map = true;
         }
         // Sleep for map_rate_sec duration, but check frequently for
         // shutdown
         while (b_continue_session) {
             std::chrono::duration<double, std::milli> time_elapsed_msec =
                 std::chrono::high_resolution_clock::now() - start;
-            if (time_elapsed_msec >= map_rate_sec) {
+            if (time_elapsed_msec >= map_rate_sec || could_not_save_map) {
                 break;
             }
             if (map_rate_sec - time_elapsed_msec >=
