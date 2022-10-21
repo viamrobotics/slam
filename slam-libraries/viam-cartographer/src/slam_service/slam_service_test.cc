9723ceeaee95314e3819d0c673a55ca141e8c544
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-10-21 13:23:22 -0400

    Test OverwriteMapBuilderParameters

--- slam-libraries/viam-cartographer/src/slam_service/slam_service_test.cc
+++ slam-libraries/viam-cartographer/src/slam_service/slam_service_test.cc
@@ -6,9 +6,65 @@
 namespace viam {
 namespace {
 
+
 BOOST_AUTO_TEST_SUITE(SLAMService)
 
-// TODO[kat]: Test OverwriteMapBuilderParameters
+void checkCartoMapBuilderParameters(SLAMServiceImpl& slamService) {
+    auto pose_graph_options = slamService.mapBuilder.map_builder_options_.pose_graph_options();
+    auto overlapping_submaps_trimmer_2d = pose_graph_options.overlapping_submaps_trimmer_2d();
+    auto ceres_scan_matcher_options = pose_graph_options.constraint_builder_options().ceres_scan_matcher_options();
+    auto trajectory_builder_2d_options = slamService.mapBuilder.trajectory_builder_options_.trajectory_builder_2d_options();
+
+    auto tolerance = boost::test_tools::tolerance(0.00001);
+    BOOST_TEST(pose_graph_options.optimize_every_n_nodes() == slamService.optimize_every_n_nodes);
+    BOOST_TEST(trajectory_builder_2d_options.submaps_options().num_range_data() == slamService.num_range_data);
+    BOOST_TEST(trajectory_builder_2d_options.missing_data_ray_length() == slamService.missing_data_ray_length, tolerance);
+    BOOST_TEST(trajectory_builder_2d_options.max_range() == slamService.max_range, tolerance);
+    BOOST_TEST(trajectory_builder_2d_options.min_range() == slamService.min_range, tolerance);
+    if (slamService.GetActionMode() == SLAMServiceActionMode::LOCALIZING) {
+        BOOST_TEST(slamService.mapBuilder.trajectory_builder_options_.pure_localization_trimmer().max_submaps_to_keep() == slamService.max_submaps_to_keep);
+    } else {
+        BOOST_TEST(slamService.mapBuilder.trajectory_builder_options_.pure_localization_trimmer().max_submaps_to_keep() == 0);
+    }
+    if (slamService.GetActionMode() == SLAMServiceActionMode::UPDATING) {
+        BOOST_TEST(overlapping_submaps_trimmer_2d.fresh_submaps_count() == slamService.fresh_submaps_count);
+        BOOST_TEST(overlapping_submaps_trimmer_2d.min_covered_area() == slamService.min_covered_area, tolerance);
+        BOOST_TEST(overlapping_submaps_trimmer_2d.min_added_submaps_count() == slamService.min_added_submaps_count);
+    } else {
+        BOOST_TEST(overlapping_submaps_trimmer_2d.fresh_submaps_count() == 0);
+        BOOST_TEST(overlapping_submaps_trimmer_2d.min_covered_area() == 0, tolerance);
+        BOOST_TEST(overlapping_submaps_trimmer_2d.min_added_submaps_count() == 0);
+    }
+    BOOST_TEST(ceres_scan_matcher_options.occupied_space_weight() == slamService.occupied_space_weight, tolerance);
+    BOOST_TEST(ceres_scan_matcher_options.translation_weight() == slamService.translation_weight, tolerance);
+    BOOST_TEST(ceres_scan_matcher_options.rotation_weight() == slamService.rotation_weight, tolerance);
+}
+
+BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_set_values_mapping) {
+    SLAMServiceImpl slamService;
+    slamService.map_rate_sec = std::chrono::seconds(60);
+    slamService.optimize_every_n_nodes = 9999;
+    slamService.num_range_data = 9999;
+    slamService.missing_data_ray_length = 9999.99;
+    slamService.max_range = 9999.99;
+    slamService.min_range = 9999.99;
+    slamService.max_submaps_to_keep = 9999;
+    slamService.fresh_submaps_count = 9999;
+    slamService.min_covered_area = 9999.99;
+    slamService.min_added_submaps_count = 9999;
+    slamService.occupied_space_weight = 9999.99;
+    slamService.translation_weight = 9999.99;
+    slamService.rotation_weight = 9999.99;
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+}
+
+BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_check_default_values_mapping) {
+    SLAMServiceImpl slamService;
+    slamService.map_rate_sec = std::chrono::seconds(60);
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+}
 
 BOOST_AUTO_TEST_CASE(GetActionMode_mapping) {
     SLAMServiceImpl slamService;
