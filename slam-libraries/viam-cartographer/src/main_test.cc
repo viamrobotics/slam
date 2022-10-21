fa3678151868fcdfb797ddf4ce681ff8b4e81f16
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-10-21 12:34:25 -0400

    Suppress logs in tests

--- slam-libraries/viam-cartographer/src/main_test.cc
+++ slam-libraries/viam-cartographer/src/main_test.cc
@@ -1,2 +1,10 @@
 #define BOOST_TEST_MODULE cartographer tests
 #include <boost/test/included/unit_test.hpp>
+#include "glog/logging.h"
+
+struct GlobalFixture {
+  void setup() {
+        google::InitGoogleLogging("config_test");
+  }
+};
+BOOST_TEST_GLOBAL_FIXTURE( GlobalFixture );
