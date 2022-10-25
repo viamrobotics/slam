92ab3e35d16d712d25852f6f4269538977776bdb
Tess Avitabile <tess.avitabile@gmail.com>
2022-10-25 09:33:56 -0400

    reinstall openssl, use flags

--- slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
+++ slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
@@ -1,8 +1,9 @@
 echo "Installing ORB_SLAM3 external dependencies"
 brew tap viamrobotics/brews
-brew install cmake glew opencv@4 eigen boost openssl
-sudo ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
-sudo ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
-sudo ln -s /usr/local/Cellar/openssl/1.0.2j/bin/openssl /usr/local/bin/openssl
+brew install cmake glew opencv@4 eigen boost
+brew reinstall openssl
+LDFLAGS=-L/usr/local/opt/openssl/lib
+CPPFLAGS=-I/usr/local/opt/openssl/include
+PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 brew install pangolin
 sudo ln -s /opt/homebrew/Cellar/pangolin/0.8/lib/* /usr/local/lib/
