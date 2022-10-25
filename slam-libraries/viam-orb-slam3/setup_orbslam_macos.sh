ca0ea3360eb9a5f7763d30b4b1dcd6fe279c5866
Tess Avitabile <tess.avitabile@gmail.com>
2022-10-25 09:09:40 -0400

    openssl symlinks

--- slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
+++ slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
@@ -1,5 +1,8 @@
 echo "Installing ORB_SLAM3 external dependencies"
 brew tap viamrobotics/brews
-brew install cmake glew opencv@4 eigen boost pangolin openssl
-cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib
+brew install cmake glew opencv@4 eigen boost openssl
+sudo ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
+sudo ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
+sudo ln -s /usr/local/Cellar/openssl/1.0.2j/bin/openssl /usr/local/bin/openssl
+brew install pangolin
 sudo ln -s /opt/homebrew/Cellar/pangolin/0.8/lib/* /usr/local/lib/
