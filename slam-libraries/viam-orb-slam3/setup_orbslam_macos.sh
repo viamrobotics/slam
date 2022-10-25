6827921bca250129fc213a70370113f84de1d305
Tess Avitabile <tess.avitabile@gmail.com>
2022-10-25 10:18:33 -0400

    export

--- slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
+++ slam-libraries/viam-orb-slam3/setup_orbslam_macos.sh
@@ -1,8 +1,8 @@
 echo "Installing ORB_SLAM3 external dependencies"
 brew tap viamrobotics/brews
 brew install cmake glew opencv@4 eigen boost openssl
-LDFLAGS=-L/usr/local/opt/openssl@3/lib
-CPPFLAGS=-I/usr/local/opt/openssl@3/include
-PKG_CONFIG_PATH=/usr/local/opt/openssl@3/lib/pkgconfig
+export LDFLAGS=-L/usr/local/opt/openssl@3/lib
+export CPPFLAGS=-I/usr/local/opt/openssl@3/include
+export PKG_CONFIG_PATH=/usr/local/opt/openssl@3/lib/pkgconfig
 brew install pangolin
 sudo ln -s /opt/homebrew/Cellar/pangolin/0.8/lib/* /usr/local/lib/ || true
