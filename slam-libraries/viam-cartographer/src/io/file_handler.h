5fd1b27732b4242d61b813c13ddb7ac741ae70f8
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-11-11 11:37:28 -0500

    Run make format

--- slam-libraries/viam-cartographer/src/io/file_handler.h
+++ slam-libraries/viam-cartographer/src/io/file_handler.h
@@ -17,8 +17,9 @@ namespace io {
 static const int filenamePrefixLength = 6;
 static const std::string time_format = "%Y-%m-%dT%H:%M:%S.0000Z";
 
-// MakeFilenameWithTimestamp creates a filename for a provided sensor with a timestamp. The filename
-// includes the path to the file. Does not support millisecond resolution.
+// MakeFilenameWithTimestamp creates a filename for a provided sensor with a
+// timestamp. The filename includes the path to the file. Does not support
+// millisecond resolution.
 const std::string MakeFilenameWithTimestamp(std::string path_to_dir);
 
 // ListSortedFilesInDirectory returns a list of the files in the directory
