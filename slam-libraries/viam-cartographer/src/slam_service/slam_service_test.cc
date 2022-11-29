60ef63490e36f23d07efa6cee820be025417f5bd
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-11-29 15:40:29 -0500

    Add tests

--- slam-libraries/viam-cartographer/src/slam_service/slam_service_test.cc
+++ slam-libraries/viam-cartographer/src/slam_service/slam_service_test.cc
@@ -1,4 +1,5 @@
 #include "slam_service.h"
+#include "../utils/test_helpers.h"
 
 #include <boost/test/unit_test.hpp>
 #include <exception>
@@ -55,8 +56,8 @@ void checkCartoMapBuilderParameters(SLAMServiceImpl& slamService) {
 }
 
 BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_set_values_mapping) {
+    // Mapping is the default action_mode when slamService is created
     SLAMServiceImpl slamService;
-    slamService.map_rate_sec = std::chrono::seconds(60);
     slamService.optimize_every_n_nodes = 9999;
     slamService.num_range_data = 9998;
     slamService.missing_data_ray_length = 9997.77;
@@ -69,22 +70,150 @@ BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_set_values_mapping) {
     slamService.occupied_space_weight = 9990.09;
     slamService.translation_weight = 9989.89;
     slamService.rotation_weight = 9988.88;
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::MAPPING);
     slamService.OverwriteMapBuilderParameters();
     checkCartoMapBuilderParameters(slamService);
 }
 
 BOOST_AUTO_TEST_CASE(
     OverwriteMapBuilderParameters_check_default_values_mapping) {
+    // Mapping is the default action_mode when slamService is created
     SLAMServiceImpl slamService;
-    slamService.map_rate_sec = std::chrono::seconds(60);
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::MAPPING);
     slamService.OverwriteMapBuilderParameters();
     checkCartoMapBuilderParameters(slamService);
 }
 
+BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_set_values_updating) {
+    SLAMServiceImpl slamService;
+    slamService.optimize_every_n_nodes = 9999;
+    slamService.num_range_data = 9998;
+    slamService.missing_data_ray_length = 9997.77;
+    slamService.max_range = 9996.66;
+    slamService.min_range = 9995.55;
+    slamService.max_submaps_to_keep = 9994;
+    slamService.fresh_submaps_count = 9993;
+    slamService.min_covered_area = 9992.22;
+    slamService.min_added_submaps_count = 9991;
+    slamService.occupied_space_weight = 9990.09;
+    slamService.translation_weight = 9989.89;
+    slamService.rotation_weight = 9988.88;
+
+    // Set the action mode to updating by providing an apriori map
+    // and by setting map_rate_sec != 0
+    slamService.map_rate_sec = std::chrono::seconds(60);
+
+    // Create a temp directory with a few files with timestamps
+    std::vector<std::string> data_files{};
+    std::vector<std::string> map_files{"map_data_2022-02-11T01:44:53.1903Z.pbstream"};
+    // Create a unique path in the temp directory and add the files
+    boost::filesystem::path tmpdir = utils::createTmpDirectoryAndAddFiles(data_files, map_files);
+    slamService.path_to_map = tmpdir.string() + "/map";
+    slamService.DetermineActionMode();
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::UPDATING);
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+
+    // Remove the temporary directory and its contents
+    utils::removeTmpDirectory(tmpdir);
+}
+
+BOOST_AUTO_TEST_CASE(
+    OverwriteMapBuilderParameters_check_default_values_updating) {
+    SLAMServiceImpl slamService;
+    // Set the action mode to updating by providing an apriori map
+    // and by setting map_rate_sec != 0
+    slamService.map_rate_sec = std::chrono::seconds(60);
+
+    // Create a temp directory with a few files with timestamps
+    std::vector<std::string> data_files{};
+    std::vector<std::string> map_files{"map_data_2022-02-11T01:44:53.1903Z.pbstream"};
+    // Create a unique path in the temp directory and add the files
+    boost::filesystem::path tmpdir = utils::createTmpDirectoryAndAddFiles(data_files, map_files);
+    slamService.path_to_map = tmpdir.string() + "/map";
+    slamService.DetermineActionMode();
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::UPDATING);
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+
+    // Remove the temporary directory and its contents
+    utils::removeTmpDirectory(tmpdir);
+}
+
+BOOST_AUTO_TEST_CASE(OverwriteMapBuilderParameters_set_values_localizing) {
+    SLAMServiceImpl slamService;
+    slamService.optimize_every_n_nodes = 9999;
+    slamService.num_range_data = 9998;
+    slamService.missing_data_ray_length = 9997.77;
+    slamService.max_range = 9996.66;
+    slamService.min_range = 9995.55;
+    slamService.max_submaps_to_keep = 9994;
+    slamService.fresh_submaps_count = 9993;
+    slamService.min_covered_area = 9992.22;
+    slamService.min_added_submaps_count = 9991;
+    slamService.occupied_space_weight = 9990.09;
+    slamService.translation_weight = 9989.89;
+    slamService.rotation_weight = 9988.88;
+
+    // Set the action mode to localizing by providing an apriori map
+    // and by setting map_rate_sec == 0
+    slamService.map_rate_sec = std::chrono::seconds(0);
+
+    // Create a temp directory with a few files with timestamps
+    std::vector<std::string> data_files{};
+    std::vector<std::string> map_files{"map_data_2022-02-11T01:44:53.1903Z.pbstream"};
+    // Create a unique path in the temp directory and add the files
+    boost::filesystem::path tmpdir = utils::createTmpDirectoryAndAddFiles(data_files, map_files);
+    slamService.path_to_map = tmpdir.string() + "/map";
+    slamService.DetermineActionMode();
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::LOCALIZING);
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+
+    // Remove the temporary directory and its contents
+    utils::removeTmpDirectory(tmpdir);
+}
+
+BOOST_AUTO_TEST_CASE(
+    OverwriteMapBuilderParameters_check_default_values_localizing) {
+    SLAMServiceImpl slamService;
+
+    // Set the action mode to localizing by providing an apriori map
+    // and by setting map_rate_sec == 0
+    slamService.map_rate_sec = std::chrono::seconds(0);
+
+    // Create a temp directory with a few files with timestamps
+    std::vector<std::string> data_files{};
+    std::vector<std::string> map_files{"map_data_2022-02-11T01:44:53.1903Z.pbstream"};
+    // Create a unique path in the temp directory and add the files
+    boost::filesystem::path tmpdir = utils::createTmpDirectoryAndAddFiles(data_files, map_files);
+    slamService.path_to_map = tmpdir.string() + "/map";
+    slamService.DetermineActionMode();
+
+    BOOST_TEST(slamService.GetActionMode() ==
+                   SLAMServiceActionMode::LOCALIZING);
+    slamService.OverwriteMapBuilderParameters();
+    checkCartoMapBuilderParameters(slamService);
+
+    // Remove the temporary directory and its contents
+    utils::removeTmpDirectory(tmpdir);
+}
+
+// TODO: Fix this test
 BOOST_AUTO_TEST_CASE(GetActionMode_mapping) {
     SLAMServiceImpl slamService;
     slamService.map_rate_sec = std::chrono::seconds(60);
-    BOOST_TEST((slamService.GetActionMode() == SLAMServiceActionMode::MAPPING));
+    BOOST_TEST(slamService.GetActionMode() == SLAMServiceActionMode::MAPPING);
 }
 
 /*
@@ -108,6 +237,37 @@ https://docs.google.com/document/d/1RsT-c0QOtkMKa-rwUGY0-emUmKIRSaO3mzT1v6MtFjk/
 SLAMServiceActionMode::LOCALIZING));
 }
 */
+
+
+// ProcessDataAndStartSavingMaps
+
+// GetNextDataFile
+
+// GetNextDataFileOffline
+
+// GetNextDataFileOnline
+
+// RunSLAM
+
+// DetermineActionMode
+// How to create different action modes:
+// Create a slamService object --> Mapping: Default.
+// Provide a path (path_to_map) that contains a map (.pbstream) --> Updating or Localizing
+// --> Set map_rate_sec == 0 --> Localizing
+// --> Set map_rate_sec != 0 --> Updating
+// Test for the error: Set map_rate_sec == 0 but don't provide a map
+
+
+
+// GetActionMode
+
+// SetUpMapBuilder
+
+// OverwriteMapBuilderParameters with different values for action mode
+
+// PaintMap
+
+// ExtractPointCloudToBuffer
 
 BOOST_AUTO_TEST_SUITE_END()
 
