1b1400faa1b70aa84584728b606e91d1d61e568e
jeremyhyde-viam <92594844+jeremyhyde-viam@users.noreply.github.com>
2023-01-09 16:38:18 -0500

    RSDK-1625 Remove Backwards Compatibility with use_live_data (#132)

--- slam-libraries/viam-orb-slam3/orbslam_server_v1_test.cc
+++ slam-libraries/viam-orb-slam3/orbslam_server_v1_test.cc
@@ -322,29 +322,18 @@ BOOST_AUTO_TEST_CASE(ParseAndValidateArguments_config_invalid_use_live_data) {
     checkParseAndValidateArgumentsException(args, message);
 }
 
-// TODO: Remove no use_live_data test cases once integration tests have been
-// updated (See associated JIRA ticket:
-// https://viam.atlassian.net/browse/RSDK-1625)
-BOOST_AUTO_TEST_CASE(ParseAndValidateArguments_online_with_no_live_data) {
-    const vector<string> args{
-        "-data_dir=/path/to", "-config_param={mode=rgbd}",
-        "-port=20000",        "-sensors=color",
-        "-data_rate_ms=200",  "-delete_processed_data=false",
-        "-map_rate_sec=60"};
-    SLAMServiceImpl slamService;
-    utils::ParseAndValidateArguments(args, slamService);
-    BOOST_TEST(slamService.use_live_data == true);
-}
-
-BOOST_AUTO_TEST_CASE(ParseAndValidateArguments_offline_with_no_live_data) {
-    const vector<string> args{
-        "-data_dir=/path/to", "-config_param={mode=rgbd}",
-        "-port=20000",        "-sensors=",
-        "-data_rate_ms=200",  "-delete_processed_data=false",
-        "-map_rate_sec=60"};
-    SLAMServiceImpl slamService;
-    utils::ParseAndValidateArguments(args, slamService);
-    BOOST_TEST(slamService.use_live_data == false);
+BOOST_AUTO_TEST_CASE(ParseAndValidateArguments_config_no_use_live_data) {
+    const vector<string> args{"-data_dir=/path/to",
+                              "-config_param={mode=rgbd}",
+                              "-port=20000",
+                              "-sensors=color",
+                              "-data_rate_ms=200",
+                              "-map_rate_sec=60",
+                              "-delete_processed_data=false",
+                              "-use_live_data="};
+    const string message =
+        "invalid use_live_data value, set to either true or false";
+    checkParseAndValidateArgumentsException(args, message);
 }
 
 BOOST_AUTO_TEST_CASE(ReadTimeFromTimestamp_missing_timestamp) {
