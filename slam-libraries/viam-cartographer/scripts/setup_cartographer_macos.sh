19cb120a47aa2981670fbe78853b1cf9ac81d9f5
zaporter-work <work-github@zackporter.com>
2023-02-24 15:52:01 -0500

    [RSDK-2012, RSDK-2013] Remove slam libraries

--- slam-libraries/viam-cartographer/scripts/setup_cartographer_macos.sh
+++
@@ -1,8 +0,0 @@
-echo "Installing cartographer external dependencies"
-brew update
-brew upgrade
-brew install abseil boost ceres-solver protobuf ninja cairo googletest lua@5.3
-brew link lua@5.3
-brew install openssl eigen gflags glog suite-sparse sphinx-doc pcl
-brew link protobuf
-brew link openssl
