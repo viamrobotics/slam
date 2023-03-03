fddc68bb756357f74576c63108f8b08658f5015b
zaporter-work <work-github@zackporter.com>
2023-03-03 15:56:45 -0500

    RSDK-2007 write image to file

--- dataprocess/dataprocess.go
+++ dataprocess/dataprocess.go
@@ -1,51 +1,59 @@
+// Package dataprocess manages code related to the data-accepting process
 package dataprocess
 
 import (
-    "os"
-    "bufio"
-    "image"
-    "context"
+	"bufio"
+	"bytes"
+	"context"
+	"image"
+	"image/png"
+	"os"
+
 	"github.com/pkg/errors"
 	"go.viam.com/rdk/rimage"
 	rdkutils "go.viam.com/rdk/utils"
 )
 
 func convertImageToPNG(ctx context.Context, img image.Image) ([]byte, error) {
-    if lazyImg, ok := img.(*rimage.LazyEncodedImage); ok {
+	if lazyImg, ok := img.(*rimage.LazyEncodedImage); ok {
 		if lazyImg.MIMEType() != rdkutils.MimeTypePNG {
 			return nil, errors.Errorf("expected mime type %v, got %T", rdkutils.MimeTypePNG, img)
 		}
-		return lazyImg.RawData(),  nil
+		return lazyImg.RawData(), nil
 	}
 
 	if ycbcrImg, ok := img.(*image.YCbCr); ok {
 		pngImage, err := rimage.EncodeImage(ctx, ycbcrImg, rdkutils.MimeTypePNG)
 		if err != nil {
-			return nil,  err
+			return nil, err
 		}
-		return pngImage,  nil
+		return pngImage, nil
 	}
-    return nil, errors.New("Failed to convert image to PNG")
+	buf := new(bytes.Buffer)
+	err := png.Encode(buf, img)
+	if err == nil {
+		return buf.Bytes(), nil
+	}
+	return nil, errors.New("Failed to convert image to PNG")
 }
 
-func WriteImageToPNGFile(ctx context.Context,image image.Image, filename string) error {
-
-    png,err := convertImageToPNG(ctx,image)
-        if err != nil {
-            return err
-        }
-        //nolint:gosec
-		f, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		w := bufio.NewWriter(f)
-		if _, err := w.Write(png); err != nil {
-			return err
-		}
-		if err := w.Flush(); err != nil {
-			return err
-		}
-		return f.Close()
-
+// WriteImageToPNGFile attempts to convert an image to PNG and then save it to the passed filename.
+func WriteImageToPNGFile(ctx context.Context, image image.Image, filename string) error {
+	png, err := convertImageToPNG(ctx, image)
+	if err != nil {
+		return err
+	}
+	//nolint:gosec
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	w := bufio.NewWriter(f)
+	if _, err := w.Write(png); err != nil {
+		return err
+	}
+	if err := w.Flush(); err != nil {
+		return err
+	}
+	return f.Close()
 }
--- dataprocess/dataprocess_test.go
+++ dataprocess/dataprocess_test.go
@@ -1,23 +1,28 @@
 package dataprocess
 
 import (
+	"context"
+	"image"
+	"os"
 	"testing"
-    "os"
-    "image"
+
 	"github.com/edaniels/golog"
 	"go.viam.com/test"
 )
 
 func TestWriteImage(t *testing.T) {
-	logger := golog.NewTestLogger(t)
-    tempDir, err := os.MkdirTemp();
-    test.That(t, err, test.ShouldBeNil)
-    img := image.NewRGBA(image.Rectangle{
-        image.Point{0, 0},
-        image.Point{200,200}
-    })
-
-
-
+	_ = golog.NewTestLogger(t)
+	tempDir, err := os.MkdirTemp("", "*")
+	defer os.RemoveAll(tempDir)
+	test.That(t, err, test.ShouldBeNil)
+	img := image.NewRGBA(image.Rectangle{
+		image.Point{0, 0},
+		image.Point{200, 200},
+	})
+	imgDest := tempDir + "test_img.png"
+	err = WriteImageToPNGFile(context.Background(), img, imgDest)
+	test.That(t, err, test.ShouldBeNil)
+	// Test that the file was actually written
+	_, err = os.Stat(imgDest)
+	test.That(t, err, test.ShouldBeNil)
 }
-
