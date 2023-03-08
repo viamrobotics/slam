7767d71088f81c824bc12f3525d7869b99d6976c
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-03-08 14:51:58 -0500

    Rename function

--- utils/utils.go
+++ utils/utils.go
@@ -3,16 +3,15 @@ package utils
 
 import "strings"
 
-// CreateKeyValuePairs converts a dictionary to a string so
+// DictToString converts a dictionary to a string so
 // it can be loaded into an arg for the slam process.
-func CreateKeyValuePairs(m map[string]string) string {
+func DictToString(m map[string]string) string {
 	stringMapList := make([]string, len(m))
 	i := 0
 	for k, val := range m {
 		stringMapList[i] = k + "=" + val
 		i++
 	}
-
 	stringMap := strings.Join(stringMapList, ",")
 
 	return "{" + stringMap + "}"
