fb5056a64a36dc9e8eddcc5dc9fdc4cf9ea751b0
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-03-14 15:38:00 -0400

    Add Check... and tests

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,12 @@
 // Package utils contains helper functions for the slam library implementations
 package utils
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"go.viam.com/rdk/spatialmath"
+)
 
 // DictToString converts a dictionary to a string so
 // it can be loaded into an arg for the slam process.
@@ -15,4 +20,27 @@ func DictToString(m map[string]string) string {
 	stringMap := strings.Join(stringMapList, ",")
 
 	return "{" + stringMap + "}"
+}
+
+// CheckQuaternionFromClientAlgo checks to see if the internal SLAM algorithm sent a quaternion. If it did, return the updated pose.
+func CheckQuaternionFromClientAlgo(pose spatialmath.Pose, componentReference string,
+	returnedExt map[string]interface{},
+) (spatialmath.Pose, string, error) {
+	// check if extra contains a quaternion. If no quaternion is found, throw an error
+	if val, ok := returnedExt["quat"]; ok {
+		q := val.(map[string]interface{})
+
+		valReal, ok1 := q["real"].(float64)
+		valIMag, ok2 := q["imag"].(float64)
+		valJMag, ok3 := q["jmag"].(float64)
+		valKMag, ok4 := q["kmag"].(float64)
+
+		if !ok1 || !ok2 || !ok3 || !ok4 {
+			return nil, "", errors.Errorf("error getting SLAM position: quaternion given, but invalid format detected, %v", q)
+		}
+		actualPose := spatialmath.NewPose(pose.Point(),
+			&spatialmath.Quaternion{Real: valReal, Imag: valIMag, Jmag: valJMag, Kmag: valKMag})
+		return actualPose, componentReference, nil
+	}
+	return nil, "", errors.Errorf("error getting SLAM position: quaternion not given, %v", returnedExt)
 }
