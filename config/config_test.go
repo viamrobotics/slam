3c1204fa4057af927b2e358099c92570da18f503
Nick Sanford <nick.sanford@viam.com>
2023-04-12 12:13:27 -0400

    move tests to validate

--- config/config_test.go
+++ config/config_test.go
@@ -7,12 +7,94 @@ import (
 	"go.viam.com/rdk/config"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/test"
+	"go.viam.com/utils"
 )
 
 var (
 	_true  = true
 	_false = false
 )
+
+func TestValidate(t *testing.T) {
+	testCfgPath := "services.slam.attributes.fake"
+	logger := golog.NewTestLogger(t)
+
+	t.Run("Empty config", func(t *testing.T) {
+		model := resource.NewDefaultModel(resource.ModelName("test"))
+		cfgService := config.Service{Name: "test", Type: "slam", Model: model}
+		_, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeError, newError("error validating \"services.slam.attributes.fake\": \"config_params[mode]\" is required"))
+	})
+
+	t.Run("Simplest valid config", func(t *testing.T) {
+		cfgService := makeCfgService()
+		_, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeNil)
+	})
+
+	t.Run("Config without required fields", func(t *testing.T) {
+		// Test for missing main attribute fields
+		requiredFields := []string{"data_dir", "use_live_data"}
+		for _, requiredField := range requiredFields {
+			logger.Debugf("Testing SLAM config without %s", requiredField)
+			cfgService := makeCfgService()
+			delete(cfgService.Attributes, requiredField)
+			_, err := newAttrConfig(cfgService)
+			test.That(t, err, test.ShouldBeError, newError(utils.NewConfigValidationFieldRequiredError(testCfgPath, requiredField).Error()))
+		}
+		// Test for missing config_params attributes
+		logger.Debug("Testing SLAM config without config_params[mode]")
+		cfgService := makeCfgService()
+		delete(cfgService.Attributes["config_params"].(map[string]string), "mode")
+		_, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeError, newError(utils.NewConfigValidationFieldRequiredError(testCfgPath, "config_params[mode]").Error()))
+	})
+
+	t.Run("Config with invalid parameter type", func(t *testing.T) {
+		cfgService := makeCfgService()
+		cfgService.Attributes["use_live_data"] = "true"
+		_, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeError, newError("1 error(s) decoding:\n\n* 'use_live_data' expected type 'bool', got unconvertible type 'string', value: 'true'"))
+		cfgService.Attributes["use_live_data"] = true
+		_, err = newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeNil)
+	})
+
+	t.Run("Config with out of range values", func(t *testing.T) {
+		cfgService := makeCfgService()
+		cfgService.Attributes["data_rate_msec"] = -1
+		_, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeError, newError("cannot specify data_rate_msec less than zero"))
+		cfgService.Attributes["data_rate_msec"] = 1
+		cfgService.Attributes["map_rate_sec"] = -1
+		_, err = newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeError, newError("cannot specify map_rate_sec less than zero"))
+	})
+
+	t.Run("All parameters e2e", func(t *testing.T) {
+		cfgService := makeCfgService()
+		cfgService.Attributes["sensors"] = []string{"a", "b"}
+		cfgService.Attributes["data_rate_msec"] = 1001
+		cfgService.Attributes["map_rate_sec"] = 1002
+		cfgService.Attributes["port"] = "47"
+		cfgService.Attributes["delete_processed_data"] = true
+
+		cfgService.Attributes["config_params"] = map[string]string{
+			"mode":    "test mode",
+			"value":   "0",
+			"value_2": "test",
+		}
+		cfg, err := newAttrConfig(cfgService)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, cfg.DataDirectory, test.ShouldEqual, cfgService.Attributes["data_dir"])
+		test.That(t, *cfg.UseLiveData, test.ShouldEqual, cfgService.Attributes["use_live_data"])
+		test.That(t, cfg.Sensors, test.ShouldResemble, cfgService.Attributes["sensors"])
+		test.That(t, cfg.DataRateMsec, test.ShouldEqual, cfgService.Attributes["data_rate_msec"])
+		test.That(t, *cfg.MapRateSec, test.ShouldEqual, cfgService.Attributes["map_rate_sec"])
+		test.That(t, cfg.Port, test.ShouldEqual, cfgService.Attributes["port"])
+		test.That(t, cfg.ConfigParams, test.ShouldResemble, cfgService.Attributes["config_params"])
+	})
+}
 
 func TestDetermineDeleteProcessedData(t *testing.T) {
 	logger := golog.NewTestLogger(t)
@@ -121,7 +203,6 @@ func newAttrConfig(cfg config.Service) (*AttrConfig, error) {
 		return &AttrConfig{}, newError(err.Error())
 	}
 
-	// This temporary value will be replaced once we are using rdk's validation
 	if _, err := attrCfg.Validate("services.slam.attributes.fake"); err != nil {
 		return &AttrConfig{}, newError(err.Error())
 	}
