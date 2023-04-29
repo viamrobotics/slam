5bdde00caa61086d9bac73c152467d53948e4a93
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-04-29 13:38:58 -0400

    Add & fix tests

--- sensors/lidar/lidar_test.go
+++ sensors/lidar/lidar_test.go
@@ -1,12 +1,18 @@
 package lidar_test
 
 import (
+	"context"
+	"errors"
+	"image/color"
 	"testing"
 
 	"go.viam.com/rdk/components/camera"
-	"go.viam.com/rdk/registry"
-	"go.viam.com/slam/sensors/lidar"
+	"go.viam.com/rdk/pointcloud"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/testutils/inject"
 	"go.viam.com/test"
+
+	"go.viam.com/slam/sensors/lidar"
 )
 
 const (
@@ -14,39 +20,128 @@ const (
 	wrongLidarName = "wrongLidarName"
 )
 
-// Test:
-// New function: empty dependencies, wrong name, successful creation of Lidar
-// GetData: get next pointcloud
-
-type mockLidar struct {
-	camera.Camera
-	Name string
-}
-
 func TestNew(t *testing.T) {
-	t.Run("Empty dependencies empty sensors failure", func(t *testing.T) {
-		deps := make(registry.Dependencies)
+	t.Run("Empty sensors array failure", func(t *testing.T) {
+		cam := &inject.Camera{}
+		deps := make(resource.Dependencies)
+		deps[camera.Named(testLidarName)] = cam
 		sensors := []string{}
-		lidar, err := lidar.New(deps, sensors, 0)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, lidar, test.ShouldNotBeNil)
+		sensorIndex := 0
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldEqual, "index out of bounds")
+		test.That(t, testLidar, test.ShouldResemble, lidar.Lidar{})
+	})
+
+	t.Run("Out of bounds index failure", func(t *testing.T) {
+		deps := make(resource.Dependencies)
+		sensors := []string{testLidarName}
+		sensorIndex := 1
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldEqual, "index out of bounds")
+		test.That(t, testLidar, test.ShouldResemble, lidar.Lidar{})
+	})
+
+	t.Run("Empty dependencies empty sensors failure", func(t *testing.T) {
+		deps := make(resource.Dependencies)
+		sensors := []string{}
+		sensorIndex := 0
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldEqual, "index out of bounds")
+		test.That(t, testLidar, test.ShouldResemble, lidar.Lidar{})
 	})
 
 	t.Run("Empty dependencies non-empty sensors failure", func(t *testing.T) {
-		deps := make(registry.Dependencies)
+		deps := make(resource.Dependencies)
 		sensors := []string{testLidarName}
-		lidar, err := lidar.New(deps, sensors, 0)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, lidar, test.ShouldNotBeNil)
+		sensorIndex := 0
+		expectedErrorMessage := "error getting lidar camera " + testLidarName + " for slam service"
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldContainSubstring, expectedErrorMessage)
+		test.That(t, testLidar, test.ShouldResemble, lidar.Lidar{})
 	})
 
 	t.Run("Wrong sensor name failure", func(t *testing.T) {
-		deps := make(registry.Dependencies)
-		deps[camera.Named(testLidarName)] = &mockLidar{Name: testLidarName}
+		deps := make(resource.Dependencies)
 		sensors := []string{wrongLidarName}
-		lidar, err := lidar.New(deps, sensors, 0)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, lidar, test.ShouldNotBeNil)
+		sensorIndex := 0
+		expectedErrorMessage := "error getting lidar camera " + wrongLidarName + " for slam service"
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldContainSubstring, expectedErrorMessage)
+		test.That(t, testLidar, test.ShouldResemble, lidar.Lidar{})
 	})
 
+	t.Run("Successful creation of Lidar", func(t *testing.T) {
+		cam := &inject.Camera{}
+		deps := make(resource.Dependencies)
+		deps[camera.Named(testLidarName)] = cam
+		sensors := []string{testLidarName}
+		sensorIndex := 0
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, testLidar, test.ShouldNotBeNil)
+		test.That(t, testLidar.Name, test.ShouldEqual, testLidarName)
+	})
+}
+
+func TestGetData(t *testing.T) {
+	t.Run("NextPointCloud not implemented failure", func(t *testing.T) {
+		expectedErrorMessage := "NextPointCloudFunc not implemented"
+		cam := &inject.Camera{}
+		cam.NextPointCloudFunc = func(ctx context.Context) (pointcloud.PointCloud, error) {
+			return nil, errors.New(expectedErrorMessage)
+		}
+		deps := make(resource.Dependencies)
+		deps[camera.Named(testLidarName)] = cam
+		sensors := []string{testLidarName}
+		sensorIndex := 0
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, testLidar, test.ShouldNotBeNil)
+		test.That(t, testLidar.Name, test.ShouldEqual, testLidarName)
+
+		pc, err := testLidar.GetData(context.Background())
+		test.That(t, pc, test.ShouldBeNil)
+		test.That(t, err, test.ShouldNotBeNil)
+		test.That(t, err.Error(), test.ShouldEqual, expectedErrorMessage)
+	})
+
+	t.Run("Successful GetData call", func(t *testing.T) {
+		cam := &inject.Camera{}
+		cam.NextPointCloudFunc = func(ctx context.Context) (pointcloud.PointCloud, error) {
+			pc1 := pointcloud.NewWithPrealloc(1)
+			err := pc1.Set(pointcloud.NewVector(1, 0, 0), pointcloud.NewColoredData(color.NRGBA{255, 0, 0, 255}))
+			test.That(t, err, test.ShouldBeNil)
+			return pc1, nil
+		}
+		deps := make(resource.Dependencies)
+		deps[camera.Named(testLidarName)] = cam
+		sensors := []string{testLidarName}
+		sensorIndex := 0
+
+		testLidar, err := lidar.New(deps, sensors, sensorIndex)
+		test.That(t, err, test.ShouldBeNil)
+		test.That(t, testLidar, test.ShouldNotBeNil)
+		test.That(t, testLidar.Name, test.ShouldEqual, testLidarName)
+
+		pc, err := testLidar.GetData(context.Background())
+		test.That(t, pc, test.ShouldNotBeNil)
+		test.That(t, pc.Size(), test.ShouldEqual, 1)
+		test.That(t, err, test.ShouldBeNil)
+
+		data, got := pc.At(1, 0, 0)
+		test.That(t, got, test.ShouldBeTrue)
+		test.That(t, data.Color(), test.ShouldResemble, &color.NRGBA{255, 0, 0, 255})
+	})
 }
